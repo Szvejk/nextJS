fdfbc3fce94a1082bc2c36339449daa4add5b44c
Sara Szwejkowska <saraszw@op.pl>
2023-04-22 14:40:11 +0200

    created atoms folder

---
+++ vuestudia/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<main class="slider">
+		<router-view />
+	</main>
+</template>
+
+<script>
+export default {
+	name: 'App',
+};
+</script>
+
+<style lang="scss">
+*,
+*::after,
+*::before {
+	margin: 0;
+	padding: 0;
+	box-sizing: inherit;
+}
+html {
+	font-size: 62.5%;
+}
+body {
+	font-family: 'Lato', sans-serif;
+	box-sizing: border-box;
+}
+.slider {
+	display: flex;
+	align-items: center;
+	position: relative;
+	width: 1140px;
+	margin-right: auto;
+	margin-left: auto;
+}
+</style>
